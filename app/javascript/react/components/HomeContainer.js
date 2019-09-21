8305b780e99524cb8cbd16856a15f860e42690a2
Anthonyhelka <helkaanthony1997@gmail.com>
2019-09-20 23:24:44 -0400

    Devise correctly set up.

--- app/javascript/react/components/HomeContainer.js
+++ app/javascript/react/components/HomeContainer.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
-import { Segment } from 'semantic-ui-react';
-
-import NavigationBar from './NavigationBar';
 
 class HomeContainer extends Component {
   render(){
     return (
       <div>
-        <NavigationBar page='home' />
       </div>
     )
   }
