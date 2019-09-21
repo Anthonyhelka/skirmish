8f3dff08768315067f31a2816727553c1efa7af0
Anthonyhelka <helkaanthony1997@gmail.com>
2019-09-21 19:55:18 -0400

    Added Username column to User table. Updated Sign in and Register forms.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root 'homes#index'
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
 
   namespace :api do
     namespace :v1 do
       get '/', to: 'homes#index'
     end
   end
-  
+
 end
