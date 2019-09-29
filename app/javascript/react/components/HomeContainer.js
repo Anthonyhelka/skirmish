1f73c403e96830fc4f2295ae38e3d2929bbc09bc
Anthonyhelka <helkaanthony1997@gmail.com>
2019-09-29 16:19:01 -0400

    Updated .env file. Added a way for React to find user account information.

--- app/javascript/react/components/HomeContainer.js
+++ app/javascript/react/components/HomeContainer.js
@@ -3,38 +3,43 @@ import { browserHistory } from 'react-router';
 
 class HomeContainer extends Component {
   constructor(props) {
-  super(props);
-  this.state={
-    signedIn: '',
-    user: ''
+    super(props);
+    this.state={
+      signedIn: false
+    }
   }
-}
 
-  // componentDidMount() {
-  //   fetch('/authentication/is_signed_in')
-  //     .then(response => {
-  //       if (response.ok) {
-  //         return response;
-  //       } else {
-  //         let errorMessage = `${response.status}(${response.statusText})`;
-  //         let error = new Error(errorMessage);
-  //         throw(error);
-  //       }
-  //     })
-  //     .then(response => response.json())
-  //     .then(body => {
-  //       this.setState({ signedIn: body.is_signed_in, user: body.user })
-  //     })
-  //     .catch(error => console.error(`Error in fetch: ${error.message}`));
-  // }
+  componentDidMount() {
+    fetch('/authentication/is_signed_in')
+      .then(response => {
+        if (response.ok) {
+          return response;
+        } else {
+          let errorMessage = `${response.status}(${response.statusText})`;
+          let error = new Error(errorMessage);
+          throw(error);
+        }
+      })
+      .then(response => response.json())
+      .then(body => {
+        this.setState({ signedIn: body.signed_in })
+      })
+      .catch(error => console.error(`Error in fetch: ${error.message}`));
+  }
 
 
   render(){
+    let greeting;
+    if (this.state.signedIn === false) {
+      greeting = 'You are not signed in!';
+    } else {
+      greeting = 'You are signed in!';
+    }
     return (
       <div>
-        hello from home {this.state.user.username}
+        <p>{greeting}</p>
       </div>
-    )
+    );
   }
 }
 
