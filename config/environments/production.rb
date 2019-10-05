34c27741b9608b8b46027c1928882ef02318e094
Anthonyhelka <helkaanthony1997@gmail.com>
2019-10-04 22:43:42 -0400

    Attempting to add images to email templates.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,8 +69,9 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.asset_host = ENV.fetch("HOST_DOMAIN_PRODUCTION")
 
-  ActionMailer::Base.default :from =>  "Skirmish <admin@skirmish.gg>" 
+  ActionMailer::Base.default :from =>  "Skirmish <admin@skirmish.gg>"
   ActionMailer::Base.default :reply_to => "<admin@skirmish.gg>"
   config.action_mailer.default_url_options = { :host => ENV.fetch("HOST_DOMAIN_PRODUCTION") }
 
